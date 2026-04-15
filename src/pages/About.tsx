89856e0c499ff3413c69b7f0300db264593c83db
meadgreenautosatl-dev <meadgreenautosatl@gmail.com>
2026-04-15 16:15:10 +0100

    Replace BMW X6 image with Chevrolet Corvette
    Updated the image on the About page to display a Chevrolet Corvette with improved styling and alignment.

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -95,20 +95,18 @@ export default function About() {
 
 
 
- {/* Right Side: Image - Proper alignment WITHOUT overflow */}
-        <div className="w-full md:w-1/2 flex justify-end">
-          <div className="aspect-square w-full max-w-md overflow-hidden rounded-lg shadow-2xl">
-            <img
-              src="/vehicles/COVER-IMAGE-TURO-2022-BMW-X6.png"
-              alt="BMW X6 - Mead Green Autos Luxury Vehicle"
-              className="h-full w-full object-cover"
-            />
+ {/* Right Side: Image */}
+          <div className="w-full md:w-1/2">
+            <FadeIn delay={0.2}>
+              <img
+                src="/vehicles/COVER-IMAGE-Chevrolet-Corvette.jpeg"
+                alt="Chevrolet Corvette"
+                className="rounded-lg shadow-2xl w-full object-cover aspect-[4/3]"
+              />
+            </FadeIn>
           </div>
-        </div>
-      </div>
-    </FadeIn>
-  </div>
 
+        </div>
 
 
 
